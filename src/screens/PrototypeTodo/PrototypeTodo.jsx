f921fdf7fa04b6be9cbc21c6b45c207efe77f9e6
mifranco-upenn <112339889+mifranco-upenn@users.noreply.github.com>
2023-11-06 19:34:00 -0500

    new popup

--- src/screens/PrototypeTodo/PrototypeTodo.jsx
+++ src/screens/PrototypeTodo/PrototypeTodo.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import "./style.css";
+import "../AddingNewTask/style.css"
 import { Link } from "react-router-dom";
 import Popup from "reactjs-popup";
 import 'reactjs-popup/dist/index.css';
+import { useState } from "react";
 
 import { AddingNewTask } from "../AddingNewTask/AddingNewTask";
 
 
 export const PrototypeTodo = () => {
+
+  const [addingNewTask, setAddingNewTask] = useState(false);
+  const [newTask, setNewTask] = useState({
+    description: '',
+    priority: 0
+  });
+
+  const handleSubmitNewTask = () => {
+    setAddingNewTask(false)
+  }
+
+
   return (
     <div className="prototype-todo">
       <div className="navigation-bar">
@@ -49,15 +63,45 @@ export const PrototypeTodo = () => {
             <div className="text-wrapper-2">To-do</div>
             <img className="img" alt="Todo close" src="/img/todo-close.svg" />
           </div>
+
+
           <Popup trigger={<img className="add-todo-button" alt="Add todo button" src="/img/add-todo-button.png" />} modal nested>
-              <AddingNewTask />
+              {/* <AddingNewTask /> */}
+              {close => (<div className="adding-new-task">
+              <div className="text-wrapper">Add New Task</div>
+              <img className="close-button" alt="Close button" src="/img/close-button.svg" onClick={close}/>
+              <div className="priority">
+                <div className="div">Priority</div>
+                <div className="priority-box">
+                  <img className="priority-marker" alt="Priority marker" src="/img/priority-marker-green.png" />
+                  <img className="img" alt="Priority marker" src="/img/priority-marker-yellow.png" />
+                  <img className="priority-marker-red" alt="Priority marker red" src="/img/priority-marker-red.png" />
+                  <img className="dividing-line" alt="Dividing line" src="/img/dividing-line-2.svg" />
+                  <img className="dividing-line-2" alt="Dividing line" src="/img/dividing-line-2.svg" />
+                </div>
+              </div>
+              <div className="task-description">
+                <p className="p">
+                  <span className="span">Task Description </span>
+                  <span className="text-wrapper-2">*</span>
+                </p>
+                <input className="description-box" type="text" value={newTask.description} onChange={(e) => setNewTask({...newTask, description: e.target.value})}/>
+              </div>
+              <div className="add-task-button">
+                <img className="group" alt="Group" src="/img/group-14.png" onClick={close} />
+              </div>
+            </div>)}
+            
           </Popup>
           
-          <div className="todo">
-            <div className="checkbox" />
-            <p className="todo-description">Follow up with management about mail-box</p>
-            <img className="priority-marker" alt="Priority marker" src="/img/priority-marker-green.png" />
-          </div>
+          {newTask.description !== '' && 
+            <div className="todo">
+              <div className="checkbox" />
+              <p className="todo-description">{newTask.description}</p>
+              <img className="priority-marker" alt="Priority marker" src="/img/priority-marker-green.png" />
+            </div>
+          }
+          
           <div className="todo-2">
             <div className="checkbox" />
             <p className="todo-description">Follow up with management about mail-box</p>
